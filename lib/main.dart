dc731a6ee04d547aa6af088d49411a8de2c8cd3a
Kacper <kisielWrosole@gmail.com>
2021-10-27 17:38:36 +0200

    Fetching data succesfully

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,137 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+// Import the firebase_core plugin
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp( GetMaterialApp(home: App()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+/// We are using a StatefulWidget such that we only create the [Future] once,
+/// no matter how many times our widget rebuild.
+/// If we used a [StatelessWidget], in the event where [App] is rebuilt, that
+/// would re-initialize FlutterFire and make our application re-enter loading state,
+/// which is undesired.
+class App extends StatefulWidget {
+  // Create the initialization Future outside of `build`:
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _AppState createState() => _AppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _AppState extends State<App> {
+  /// The future is part of the state of our widget. We should not call `initializeApp`
+  /// directly inside [build].
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      final Controller c = Get.put(Controller());
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return ErrorPage(error: snapshot.error.toString());
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return const MainPage();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return const Loading();
+      },
     );
   }
+}
+class Tea{
+    final String description;
+    final String name;
+    final List<int> brew_time;
+
+
+    Tea(this.name, this.description, this.brew_time);
+    @override
+    String toString(){
+        String brews = "";
+        for( var i in brew_time){
+           brews+=i.toString() + ' '; 
+        }
+        return 'Tea: ${name}, ${description}, brewing times:  ${brews}';
+    }
+}
+class Controller extends GetxController {
+    Map<String, List<Tea>> teaTypes = {};
+       Future<void> deserializeData() async{
+           print('Fetching data from firebasesirestore');
+            FirebaseFirestore firebase = FirebaseFirestore.instance;
+            var snapshotTeaTypes = firebase.collection('tea_types');
+            var teaTypesDocs =  await snapshotTeaTypes.get().then((snapshot)=> snapshot.docs);
+           for( var teaType in teaTypesDocs){
+                teaTypes.addEntries({MapEntry(teaType.id, <Tea>[])});
+
+                var snapshotTea = firebase.collection('tea_types').doc(teaType.id).collection('teas');
+                var teaDocs = await snapshotTea.get().then((snapshot) => snapshot.docs);
+                for(var teaDoc in teaDocs){
+                    final snapshotBrewTime = snapshotTea.doc(teaDoc.id).collection('brews');
+                    final brewTimeDocs =  await snapshotBrewTime.get().then((snapshot)=>snapshot.docs);
+                    
+                    final brewMap = brewTimeDocs.asMap();
+                    
+                    var tea = Tea(teaDoc.data()['name'].toString(), teaDoc.data()['description'].toString(), []);
+                    for(var doc in brewMap.entries){
+                        tea.brew_time.add(int.parse(doc.value['time']));
+                    }
+                    print(tea.toString());
+                    teaTypes[teaType.id]?.add(tea);
+                }
+           }
+
+       }
+}
+class MainPage extends StatelessWidget{
+    const MainPage({Key? key}) : super(key: key);
+    
+
+
+    @override
+    Widget build(BuildContext context) {
+    Controller c = Get.find();
+        
+
+        return Center(
+                child: FutureBuilder(
+                        future: c.deserializeData(),
+                        builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
+                                return Text(c.teaTypes.toString());
+                        },
+                ),
+
+        );
+    }
+}
+class ErrorPage extends StatelessWidget{
+    const ErrorPage({Key? key, String? error}) : _error=error, super(key: key);
+    final _error;
+    @override
+    Widget build(BuildContext context) {
+        return Center(
+                child: Text('Error!!! $_error'),
+        );
+    }
+}
+class Loading extends StatelessWidget{
+    const Loading({Key? key}) : super(key: key);
+    
+    @override
+    Widget build(BuildContext context) {
+        return const Center(
+                child: Text('Loading...'),
+        );
+    }
 }
